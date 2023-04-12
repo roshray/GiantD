ef23f9ee031d844e7ee8a8a738b55f8f4e675c7c
roshray <roshanroy759@gmail.com>
2023-04-12 20:09:09 +0530

    layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,16 @@
+import Canvas from "./canvas"
+import Customizer from "./pages/Customizer.jsx"
+import Home from "./pages/Home.jsx"
 
 
 function App() {
+
   return (
-    <div>
-      <h1 className="head-text">3D</h1>
-   </div>
+    <main className="app transition-all ease-in">
+      <Home />
+      <Canvas />
+      <Customizer />
+   </main>
   )
 }
 
